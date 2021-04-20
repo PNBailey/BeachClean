88b574b8e195a8f9b06d1a0c47d502f63187fd69
Paul Bailey <52pbailey@gmail.com>
2021-04-20 07:56:43 +0100

    added friends paginated result to member detail

--- client/src/app/members/member-detail/member-detail.component.ts
+++ client/src/app/members/member-detail/member-detail.component.ts
@@ -1,8 +1,10 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { faBriefcase, faLocationArrow, faUser, faUsers } from '@fortawesome/free-solid-svg-icons';
 import { ToastrService } from 'ngx-toastr';
 import { Observable, Subscription } from 'rxjs';
 import { Member } from 'src/app/models/member';
+import { Pagination } from 'src/app/models/pagination';
 import { AccountService } from 'src/app/shared/account.service';
 import { FriendsService } from 'src/app/shared/friends.service';
 import { MemberService } from 'src/app/shared/member.service';
@@ -15,15 +17,22 @@ import { MemberService } from 'src/app/shared/member.service';
 export class MemberDetailComponent implements OnInit, OnDestroy {
 
   member: Observable<Member>;
-  // likedUsers: Member[];
+  likedUsers: Observable<Pagination>;
   // userLiked: boolean = false;
   likedUser: Member;
-  addLikeSub: Subscription;
+  addLikeSub = new Subscription();
+  faLocationArrow = faLocationArrow;
+  faUser = faUser;
+  faBriefcase = faBriefcase;
+  faUsers = faUsers;
 
   constructor(private route: ActivatedRoute, private accountService: AccountService, private toastr: ToastrService, private friendService: FriendsService, private memberService: MemberService) { }
 
   ngOnInit(): void {
-    this.member = this.memberService.getMember(this.route.snapshot.paramMap.get('username'));   
+    
+    this.member = this.memberService.getMember(this.route.snapshot.paramMap.get('username')); 
+    this.likedUsers = this.friendService.getPaginatedLikes();
+    
   }
 
   likeMember(member: Member) {
